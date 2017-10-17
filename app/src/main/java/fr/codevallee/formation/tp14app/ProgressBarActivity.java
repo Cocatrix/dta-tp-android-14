e8ee5413cb5fb9baca34679ab546703406f1ebbb
Cocatrix <cocatrix@laposte.net>
2017-10-17 15:25:00 +0200

    Thread stopped on pause

--- app/src/main/java/fr/codevallee/formation/tp14app/ProgressBarActivity.java
+++ app/src/main/java/fr/codevallee/formation/tp14app/ProgressBarActivity.java
@@ -10,9 +10,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ProgressBarActivity extends AppCompatActivity {
     private Handler handler;
     private ProgressBar pBar;
+
+    private AtomicBoolean isRunning = new AtomicBoolean(false);
+    private AtomicBoolean isPausing = new AtomicBoolean(false);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,43 +43,43 @@ public class ProgressBarActivity extends AppCompatActivity {
                     Bundle messageBundle = new Bundle();
                     while(ProgressBarActivity.this.pBar.getProgress() <
                             ProgressBarActivity.this.pBar.getMax()) {
-                        Log.d("WAIT","Sleeping");
-                        Thread.sleep(300);
-                        Message message = handler.obtainMessage();
-                        messageBundle.putInt("progress", 10);
-                        message.setData(messageBundle);
-                        handler.sendMessage(message);
+                        if (isPausing.get()) {
+                            Log.d("WAIT", "Sleeping for a while");
+                            Thread.sleep(5000);
+                        } else {
+                            Log.d("WAIT", "Sleeping a little");
+                            Thread.sleep(200);
+                            Message message = handler.obtainMessage();
+                            messageBundle.putInt("progress", 5);
+                            message.setData(messageBundle);
+                            handler.sendMessage(message);
+                        }
                     }
                 } catch (Throwable e) {
                     e.printStackTrace();
                 }
             }
         });
-        /* Does not work, tried to stop the circle rotating after it's finished
-        Thread progressCircleThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ProgressBar pCirBar = (ProgressBar) findViewById(R.id.progressBarCircle);
-                    Bundle messageBundle = new Bundle();
-                    while(ProgressBarActivity.this.pBar.getProgress() <
-                            ProgressBarActivity.this.pBar.getMax()) {
-                        Log.d("WAIT","Sleeping");
-                        pCirBar.animate();
-                        Thread.sleep(100);
-                    }
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        RotateDrawable rotateDrawable = (RotateDrawable) pCirBar.getIndeterminateDrawable();
-                        rotateDrawable.setToDegrees(0);
-                    }
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        progressCircleThread.start();
-        */
+
+        isRunning.set(true);
         progressThread.start();
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        isRunning.set(false);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        isPausing.set(true);
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+        isPausing.set(false);
+        super.onResume();
     }
 }
